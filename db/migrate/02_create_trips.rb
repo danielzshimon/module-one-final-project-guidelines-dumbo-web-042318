497ba0f206a1b4ab0bcd993d5d298e420bb3eeba
ahjuice <ahjuice@users.noreply.github.com>
2018-05-10 14:51:47 -0400

    Did DB migration

--- db/migrate/02_create_trips.rb
+++ db/migrate/02_create_trips.rb
@@ -1,4 +1,4 @@
-class CreateTrips < ActiveRecord::Migration
+class CreateTrips < ActiveRecord::Migration[5.1]
   def change
     create_table :trips do |t|
       t.string :origin
--- db/migrate/04_create_trip_lines.rb
+++ db/migrate/04_create_trip_lines.rb
@@ -1,4 +1,4 @@
-class CreateTripLines < ActiveRecord::Migration
+class CreateTripLines < ActiveRecord::Migration[5.1]
   def change
     create_table :trip_lines do |t|
     t.integer :trip_id
