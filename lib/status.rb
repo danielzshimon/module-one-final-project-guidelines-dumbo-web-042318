db25e768b778203727a169d26f40bcd51051350c
ahjuice <ahjuice@users.noreply.github.com>
2018-05-10 14:17:06 -0400

    Added .situations method for Status class

--- lib/status.rb
+++ lib/status.rb
@@ -1,10 +1,16 @@
 class Status
+  @@situations = []
+
   def self.url
     "http://web.mta.info/status/ServiceStatusSubway.xml"
   end
 
   def self.update
     hash = XmlSimple.xml_in(open(url))
-    situations = hash["ServiceDelivery"][0]["SituationExchangeDelivery"][0]["Situations"][0]["PtSituationElement"]
+    @@situations = hash["ServiceDelivery"][0]["SituationExchangeDelivery"][0]["Situations"][0]["PtSituationElement"]
+  end
+
+  def self.situations
+    @@situations
   end
 end
