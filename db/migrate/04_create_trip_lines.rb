4df0481fc7010b6596e775ee189b79b8c065f099
ahjuice <ahjuice@users.noreply.github.com>
2018-05-10 14:54:02 -0400

    Fixed indentation

--- db/migrate/04_create_trip_lines.rb
+++ db/migrate/04_create_trip_lines.rb
@@ -1,8 +1,8 @@
 class CreateTripLines < ActiveRecord::Migration[5.1]
   def change
     create_table :trip_lines do |t|
-    t.integer :trip_id
-    t.integer :line_id
+      t.integer :trip_id
+      t.integer :line_id
     end
   end
 end
